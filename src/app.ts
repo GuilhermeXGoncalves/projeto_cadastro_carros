36ce03b9bce9bc6a848cf52bde9af5f70a417acb
Guilherme <guilherme.goncalves4871@gmail.com>
2025-03-12 17:13:09 -0300

    Feat: import das rotas

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, {Request, Response,NextFunction} from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import carsRouter from "./routers/carsRouter"
 
 const app = express();
 
@@ -14,9 +15,11 @@ app.use(helmet()); //Módulo para proteger um pouco mais o nosso backend (caso e
 
 app.use(express.json()); //essa config do módulo serve para o back-end conseguir entender o JSON
 
-app.use((req: Request, res: Response, next: NextFunction)=>{
-    res.send('Backend rodando na porta 3000')
-}) //módulo de teste para ver se o backend está funcionando
+app.use("/cars", carsRouter)
+
+// app.use((req: Request, res: Response, next: NextFunction)=>{
+    //res.send('Backend rodando na porta 3000')
+// }) //módulo de teste para ver se o backend está funcionando
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => { 
     res.status(500).send(error.message)
