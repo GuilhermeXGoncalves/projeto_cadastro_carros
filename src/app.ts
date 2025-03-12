f44ed4a3c20bfd9af5d26e68166f8b04134ff784
Guilherme <guilherme.goncalves4871@gmail.com>
2025-03-12 15:45:19 -0300

    Feat: Criando primeiras rotas do servidor e importando os módulos que serão utilizados

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import "express-async-errors";
+import express, {Request, Response,NextFunction} from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(morgan("tiny")); //adicionando ao express o módulo de LOG, para ver o que está chegando até o backend
+
+app.use(cors()); //Módulo para fazer a comunicação do back-end com o front-end
+
+app.use(helmet()); //Módulo para proteger um pouco mais o nosso backend (caso estude a fundo sobre esse módulo, consigo deixar o backend mais seguro)
+
+app.use(express.json()); //essa config do módulo serve para o back-end conseguir entender o JSON
+
+app.use((req: Request, res: Response, next: NextFunction)=>{
+    res.send('Backend rodando na porta 3000')
+}) //módulo de teste para ver se o backend está funcionando
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => { 
+    res.status(500).send(error.message)
+}) //tratamento de erro geral
+
+
+export default app;
